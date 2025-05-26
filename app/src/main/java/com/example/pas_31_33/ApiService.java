f6c30c12bf2f5403eeacad5109740356f9473e21
DraniXx-PNG <rajendraghasia28@gmail.com>
2025-05-26 09:31:08 +0700

    Feat/Style/EFL_API_DRAWER_NAV/RGD

--- app/src/main/java/com/example/pas_31_33/ApiService.java
+++ app/src/main/java/com/example/pas_31_33/ApiService.java
@@ -4,9 +4,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiService {
-    @GET("search_all_teams.php?l=English%20Premier%20League")
-    Call<TeamResponse> getPremierLeagueTeams();
-
     @GET("/api/v1/json/3/search_all_teams.php?s=Soccer&c=Spain")
     Call<TeamResponse> getSpanishLeagueTeams();
 
---
+++ app/src/main/java/com/example/pas_31_33/FragmentProfile.java
@@ -0,0 +1,18 @@
+package com.example.pas_31_33;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentProfile extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return view;
+    }
+}
